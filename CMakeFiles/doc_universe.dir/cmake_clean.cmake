d4cdeb215e9dc5cad7c0dce55e20aac35280b6de
thibaultfalque <thibault.falque@gmail.com>
2023-03-17 13:37:34 +0000

    Deploying to doxygen from @ crillab/universe@00fefc4aa92cb31a792b1f7a89a79de98c5aa583 🚀

--- CMakeFiles/doc_universe.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/doc_universe"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/doc_universe.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
